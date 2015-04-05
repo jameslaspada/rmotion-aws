9602fb58b10af473c9d7a89538bf71a33b983a1d
James La Spada <james.laspada@gmail.com>
2015-04-05 10:27:55 -0400

    test connection

--- lib/rmotion-aws/aws.rb
+++ lib/rmotion-aws/aws.rb
@@ -8,9 +8,17 @@ module RMOTION
 
     def getConnection
       creds = Aws::Credentials.new(@key, @secret_key)
-      puts "creds are #{creds.set?.to_s}"
       ec2 = Aws::EC2::Resource.new(region:'us-east-1', credentials: creds)
       instances = ec2.instances
+    end
+
+    def test
+      keys = ["@key","@secret_key"]
+      instances = ["instance1","instance2"]
+      return ({
+        :keys => keys, 
+        :instances => instances
+        })
     end
   end
 end
